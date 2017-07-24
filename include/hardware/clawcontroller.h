1c6337c86b56352458c79e48f7e063a86f4c77fc
fb39ca4 <fb39ca4@gmail.com>
2017-07-24 15:11:47 -0700

    Claw controller delays

--- include/hardware/clawcontroller.h
+++ include/hardware/clawcontroller.h
@@ -5,14 +5,22 @@
 class ClawController {
 public:
   ClawController(ServoTINAH& liftServo, ServoTINAH& grabServo, uint8_t liftAngles[5], uint8_t grabAngles[5]);
-  enum LiftPosition { FOLDED, VERTICAL, TOP, MIDDLE, BOTTOM };
-  enum GrabPosition { OPEN, CLOSED };
-  void setLift(LiftPosition position);
-  void setGrab(GrabPosition position);
+  enum LiftPosition { FOLDED = 0, VERTICAL = 1, TOP = 2, MIDDLE = 3, BOTTOM = 4 };
+  enum GrabPosition { OPEN = 0, CLOSED = 1 };
+  bool setLift(LiftPosition position);
+  bool setGrab(GrabPosition position);
+  void tick();
 private:
   ServoTINAH& m_liftServo;
   ServoTINAH& m_grabServo;
   uint8_t m_liftAngles[5];
   uint8_t m_grabAngles[2]; 
+
+  uint8_t m_currentLiftAngle;
+  uint8_t m_liftAngleTarget;
+  uint8_t m_currentGrabAngle;
+  uint8_t m_grabAngleTarget;
+  const uint8_t m_liftSpeed = 10;
+  const uint8_t m_grabSpeed = 10;
 
 };
